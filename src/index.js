926184c6a9509c549158e69de55526cc9699a556
amiloszewicz <miloszewicz.adam@gmail.com>
2019-03-20 15:23:52 +0100

    feat index.js and set files to run app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
-import App from "./App";
-import style from "./main.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './Components/App/App.js';
 
-console.log(`I'm a silly entry point`);
-const arr = [1, 2, 3];
-const iAmJavascriptES6 = () => console.log(...arr);
-window.iAmJavascriptES6 = iAmJavascriptES6;
+ReactDOM.render(<App />, document.getElementById('random-quote-machine'));
 
