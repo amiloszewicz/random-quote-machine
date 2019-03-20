4acae3c0808ae49fbba0829d0999a6f88ef08785
amiloszewicz <miloszewicz.adam@gmail.com>
2019-03-20 15:02:43 +0100

    feat TwitterButton Component

---
+++ src/Components/App/Board/Buttons/TwitterButton.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class TwitterButton extends Component {
+  render() {
+    return (
+      <button>Twitter</button>
+    );
+  }
+}
+
+export default TwitterButton;
+
