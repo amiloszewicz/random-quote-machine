b2c286f639f7173cedf793769263d2d6ae352b6b
amiloszewicz <miloszewicz.adam@gmail.com>
2019-03-20 15:02:57 +0100

    feat TumblrrButton Component

---
+++ src/Components/App/Board/Buttons/TumblrButton.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class TumblrButton extends Component {
+  render() {
+    return (
+      <button>Tumblr</button>
+    );
+  }
+}
+
+export default TumblrButton;
+
