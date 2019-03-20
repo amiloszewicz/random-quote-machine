69d8e21f086a4430e64eea047e7d3be772662838
amiloszewicz <miloszewicz.adam@gmail.com>
2019-03-20 15:01:04 +0100

    feat Quote Component

---
+++ src/Components/App/Board/Quote/Quote.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+import QuoteAuthor from './QuoteAuthor/QuoteAuthor.js';
+import QuoteText from './QuoteText/QuoteText.js';
+
+class Quote extends Component {
+  render() {
+    console.log(this.props.quote);
+    return (
+      <div>
+        <QuoteText quoteText={this.props.quote.text} />
+        <QuoteAuthor quoteAuthor={this.props.quote.author} />
+      </div>
+    );
+  }
+}
+
+export default Quote;
+
