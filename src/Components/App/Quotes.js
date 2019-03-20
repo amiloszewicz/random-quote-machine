e41ddf7e17920a824b7a9749c2b734c830b1fd74
amiloszewicz <miloszewicz.adam@gmail.com>
2019-03-20 14:56:52 +0100

    feat quotes file for refactor app to react

---
+++ src/Components/App/Quotes.js
@@ -0,0 +1,105 @@
+export const quotes = [
+  {
+    quote: "a journey of a thousand miles begins with a single step",
+    author: "Laozi"
+  },
+  {
+    quote:
+      "Do not take life too seriously. You will never get out of it alive.",
+    author: "Elbert Hubbard"
+  },
+  {
+    quote:
+      "To succeed in life, you need three things: a wishbone, a backbone and a funny bone.",
+    author: "Reba McEntire"
+  },
+  {
+    quote:
+      "There are only three things women need in life: food, water, and compliments.",
+    author: "Chris Rock"
+  },
+  {
+    quote:
+      "Always remember that you are absolutely unique. Just like everyone else.",
+    author: "Margaret Mead"
+  },
+  {
+    quote: "Behind every great man is a woman rolling her eyes.",
+    author: "Jim Carrey"
+  },
+  {
+    quote: "Age is something that doesn't matter, unless you are a cheese.",
+    author: "Luis Bunuel"
+  },
+  {
+    quote: "A day without sunshine is like, you know, night.",
+    author: "Steve Martin"
+  },
+  {
+    quote:
+      "I believe that if life gives you lemons, you should make lemonade... And try to find somebody whose life has given them vodka, and have a party.",
+    author: "Ron White"
+  },
+  {
+    quote:
+      "By all means let's be open-minded, but not so open-minded that our brains drop out.",
+    author: "Richard Dawkins"
+  },
+  {
+    quote: "Man cannot live by bread alone, he must have peanut butter.",
+    author: "James A. Garfield"
+  },
+  {
+    quote:
+      "They say marriages are made in Heaven. But so is thunder and lightning.",
+    author: "Clint Eastwood"
+  },
+  {
+    quote: "I'm writing a book. I've got the page numbers done.",
+    author: "Steven Wright"
+  },
+  {
+    quote: "Never have more children than you have car windows.",
+    author: "Erma Bombeck"
+  },
+  {
+    quote: "Rice is great when you’re hungry and you want 2000 of something.",
+    author: "Mitch Hedberg"
+  },
+  {
+    quote: "Accept who you are. Unless you’re a serial killer.",
+    author: "Ellen DeGeneres "
+  },
+  {
+    quote: "If a book about failures doesn’t sell, is it a success?",
+    author: "Jerry Seinfeld"
+  },
+  {
+    quote: "Go to Heaven for the climate, Hell for the company.",
+    author: "Mark Twain"
+  },
+  {
+    quote: "Why do men like intelligent women? Because opposites attract.",
+    author: "Kathy Lette"
+  },
+  {
+    quote: "My doctor told me to watch my drinking. Now I drink in front of a mirror.",
+    author: "Rodney Dangerfield"
+  },
+  {
+    quote: "You’re only as good as your last haircut.",
+    author: "Fran Lebowitz"
+  },
+  {
+    quote: "There is nothing better than a friend, unless it is a friend with chocolate.",
+    author: "Linda Grayson"
+  },
+  {
+    quote: "If I had asked people what they wanted, they would have said faster horses.",
+    author: "Henry Ford"
+  },
+  {
+    quote: "What a kid I got, I told him about the birds and the bees and he told me about the butcher and my wife.",
+    author: "Rodney Dangerfield"
+  }
+];
